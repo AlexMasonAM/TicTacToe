45e47203b347151409f2956b92105cc6f50acab8
AlexMasonAM <alexmmason@gmail.com>
2014-12-20 17:48:14 -0800

    ttt basic logic

--- js/app.js
+++ js/app.js
@@ -1,21 +1,49 @@
 var tttApp = angular.module('tttApp', []);
 
 tttApp.controller('TttController', function($scope){
+	//WORKING mark function
+	// $scope.mark = function(){
+	// 	console.log(this);
+	// 	console.log($scope.grid[0][1].cellState.location);
+	// 	this.cell.cellState = "x";
+	// WORKING win logic
+	// 	if($scope.grid[0][0].cellState == "x" && $scope.grid[0][1].cellState  == "x" && $scope.grid[0][2].cellState  == "x")
+	// 		alert("x wins");
+	// 	else if($scope.grid[1][0].cellState == "x" && $scope.grid[1][1].cellState  == "x" && $scope.grid[1][2].cellState  == "x")
+	// 		alert("x wins");
+	// 	else if($scope.grid[2][0].cellState == "x" && $scope.grid[2][1].cellState  == "x" && $scope.grid[2][2].cellState  == "x")
+	// 		alert("x wins");
+	// 	else if($scope.grid[0][0].cellState == "x" && $scope.grid[1][1].cellState  == "x" && $scope.grid[2][2].cellState  == "x")
+	// 		alert("x wins");
+	// 	else if($scope.grid[2][0].cellState == "x" && $scope.grid[1][1].cellState  == "x" && $scope.grid[0][2].cellState  == "x")
+	// 		alert("x wins");
+
+	// };
+
+	$scope.mark = function(cell){
+		if(cell.cellState != " ") {
+			alert("that space is already taken")
+		}
+		else {
+			cell.cellState = $scope.currentPlyr();
 	
-	$scope.mark = function(){
-		console.log($scope.grid[0][1].cellState);
-		this.cell.cellState = "x";
-		if($scope.grid[0][0].cellState == "x" && $scope.grid[0][1].cellState  == "x" && $scope.grid[0][2].cellState  == "x")
-			alert("x wins");
+				console.log($scope.plyr1.plyrTurn)
+	
+				if($scope.grid[0][0].cellState + $scope.grid[0][1].cellState  + $scope.grid[0][2].cellState  == 15)
+					alert("player 1 wins");
+				else if($scope.grid[1][0].cellState + $scope.grid[1][1].cellState + $scope.grid[1][2].cellState  == 15)
+					alert("player 1 wins");
+				else if($scope.grid[2][0].cellState + $scope.grid[2][1].cellState + $scope.grid[2][2].cellState  == 15)
+					alert("player 1 wins");
+				else if($scope.grid[0][0].cellState + $scope.grid[1][1].cellState + $scope.grid[2][2].cellState  == 15)
+					alert("player 1 wins");
+				else if($scope.grid[2][0].cellState + $scope.grid[1][1].cellState + $scope.grid[0][2].cellState  == 15)
+				alert("player 1 wins")
+		}
 	};
 
 	$scope.grid = [
 		[{
-			cellState: " ",
-			location: 0
-			
-		},
-		{
 			cellState: " ",
 			location: 1
 			
@@ -24,13 +52,13 @@ tttApp.controller('TttController', function($scope){
 			cellState: " ",
 			location: 2
 			
-		}],
-		[{
+		},
+		{
 			cellState: " ",
 			location: 3
 			
-		},
-		{
+		}],
+		[{
 			cellState: " ",
 			location: 4
 			
@@ -39,22 +67,31 @@ tttApp.controller('TttController', function($scope){
 			cellState: " ",
 			location: 5
 			
-		}],
-		[{
+		},
+		{
 			cellState: " ",
 			location: 6
 			
-		},
-		{
+		}],
+		[{
 			cellState: " ",
 			location: 7
 			
 		},
 		{
-			cellState: " "
+			cellState: " ",
+			location: 8
+			
+		},
+		{
+			cellState: " ",
+			location: 9
 			
 		},
 		]];	
+
+
+	console.log($scope.grid)
 
 	$scope.winConditions = [
 		{
@@ -63,18 +100,7 @@ tttApp.controller('TttController', function($scope){
 			winDia: [["A1","B2","C3"],["A3","B2","C1"]],
 		}];	
 
-	$scope.plyr1 = {
-		plyrTurn: false,
-		plyrMark: 3,
-		stack: $scope.stack
-
-	};
-	
-	$scope.plyr2 = {
-		plyrTurn: false,
-		plyrMark: 5,
-		stack: $scope.stack
-	};
+ç
 
 	$scope.stack = [
 		{
@@ -98,9 +124,52 @@ tttApp.controller('TttController', function($scope){
 		},
 	]
 
-});	// end of my controller
+	$scope.scoreboard = {
+		games: [],
+		rounds: []
+	}
+
+	$scope.currentPlyr = function(){
+		var play = "";
+		if($scope.plyr1.plyrTurn == true){
+			play = $scope.plyr1.plyrMark;
+			$scope.plyr1.plyrTurn = false;
+			$scope.plyr2.plyrTurn = true;
+		}
+		else if($scope.plyr2.plyrTurn == true){
+			play = $scope.plyr2.plyrMark;
+			$scope.plyr1.plyrTurn = true;
+			$scope.plyr2.plyrTurn = false;
+		}
+		return play;
+	}
+
+	$scope.game = function(){} //assigned to player that wins game : game winner determined by winConditions
+	$scope.round = function(){} //assigned to player that wins the round : determined by player that wins the most games in a round: tiebreak goes to winner of RoChamBeau for the round.
+	$scope.match = function(){} // assigned to player that wins the match: determined by player that wins the most rounds in a match
 
 	
+
+
+});	// end of my controller
+
+// switch between players need to change variables//
+// $scope.selectSquare = function() {
+// 		console.log($scope.currentPlayer);
+		
+// 		player1 = "x";
+// 		player2 = "o";
+
+// 		if ($scope.currentPlayer == 'x' && player1 = "x") {
+// 			this.cell.pick = "x";
+// 			$scope.currentPlayer = 'o';
+// 		} else {
+// 			$scope.currentPlayer = 'o' && player2= "o";
+// 			this.cell.pick = "o";
+// 			$scope.currentPlayer = 'x';
+// 			};
+// 		return $scope.currentPlayer;
+// 	};	
 
 /*
 tttApp.controller("TttController",['$scope', function($scope){
@@ -124,3 +193,26 @@ tttApp.controller("TttController",['$scope', function($scope){
 		image: "../img/o-mark.png";
 	};
 */
+
+
+
+	// $scope.count = function(){
+	// 	if (player = x){
+	// 		if(count = 0){
+	// 			count = count + 1;
+	// 			return count;
+	// 			}
+	// 		}
+	// 	if (player = y){
+	// 		if(count = 0){
+	// 			count = count = -1;
+	// 			return count;
+	// 			}
+	// 		}
+	// 	}
+
+// $scope.mark = function(){
+// 		console.log(this);
+// 		//console.log($scope.grid[0][1].cellState.location); unnecessary console.log
+// 		this.cell.cellState = 5;
+
